c127ef72722568e51b97008432c2c2781960b5cb
Tilo Wiklund <tilo@wiklund.pm>
2018-01-29 22:16:08 +0100

    Fixed bug and added tail probability computations

--- src/main/scala/ScalaDensity.scala
+++ src/main/scala/ScalaDensity.scala
@@ -20,7 +20,7 @@ import Scalaz._
 import vegas._
 // import vegas.render.WindowRenderer._
 
-import scala.math.{min, max}
+import scala.math.{min, max, log, exp}
 import scala.math.BigInt._
 
 import scala.collection.mutable.{ HashMap, PriorityQueue }
@@ -83,7 +83,7 @@ object ScalaDensity {
   def right(x : NodeLabel) : NodeLabel = 2*x + 1
 
   def isAncestorOf(a : NodeLabel, b : NodeLabel) : Boolean =
-    (b >> (b.bitLength - a.bitLength)) == a
+    a < b && ((b >> (b.bitLength - a.bitLength)) == a)
 
   type InfiniteTree[T] = NodeLabel => T
   type FiniteTree[T] = Map[NodeLabel, T]
@@ -131,8 +131,18 @@ object ScalaDensity {
                            stoprule : (A, Long, Rectangle) => Boolean)
       : HistogramStream[A] = {
     val fullTree = binarySplitTree(points.map(point(_)).reduce(hull))
+
     val boxTree = fullTree map { case (box, _, _) => box }
     val splitsTree = fullTree map { case (_, thresh, axis) => (thresh, axis) }
+
+    // print("Sanity: ")
+    // println(boxTree(rootLabel).volume)
+    // println(boxTree(left(rootLabel)).volume + boxTree(right(rootLabel)).volume)
+    // println(boxTree(left(left(rootLabel))).volume +
+    //           boxTree(left(right(rootLabel))).volume +
+    //           boxTree(right(left(rootLabel))).volume +
+    //           boxTree(right(right(rootLabel))).volume)
+    // println("****")
 
     def go(  cells : RDD[(NodeLabel, MLVector)], // Current partition cells
          accCounts : Map[NodeLabel, Long], // Current cell counts
@@ -199,9 +209,9 @@ object ScalaDensity {
   }
 
   // Compute support carved histogram (without intermediate stops)
-  def supportCarved(      points : RDD[MLVector],
-                     splitThresh : Double,
-                          minVol : Double ) : Histogram = {
+  def supportCarved( points : RDD[MLVector],
+                splitThresh : Double,
+                     minVol : Double ) : Histogram = {
     val totalCount = points.count()
     def stoprule(noinput : Unit, count : Long, box : Rectangle) : Boolean = {
       val v = box.volume
@@ -214,11 +224,27 @@ object ScalaDensity {
 
   // Compute histogram given volume and count bounds
   def histogram( points : RDD[MLVector],
-                 splitThresh : Long,
+            splitThresh : Long,
                  minVol : Double ) : Histogram = {
     def stoprule(noinput : Unit, count : Long, box : Rectangle) : Boolean =
       (count >= splitThresh) && (box.volume > minVol)
-    histogramPartial(points, stoprule).fix(Unit).last
+    val hs = histogramPartial(points, stoprule).fix(Unit)
+    // for(h <- hs) {
+    //   println(probAtNode(h, rootLabel))
+    //   println(h.leaves.toList.map(probAtNode(h, _)).sum)
+    //   println("++++")
+    //   print("Vol: ")
+    //   println(h.leaves.toList.map(l => h.boxes(l).volume).sum)
+    //   // println("****")
+    //   // println(h.leaves.toList.map(l => (l, h.boxes(l).volume)).toMap)
+    //   println("++++")
+    //   print("Count: ")
+    //   println(h.leaves.toList.map(l => h.counts(l)).sum)
+    //   // println("****")
+    //   // println(h.leaves.toList.map(l => (l, h.counts(l))).toMap)
+    //   println("----")
+    // }
+    hs.last
   }
 
   def query(f : Histogram, p : MLVector) : NodeLabel = {
@@ -240,12 +266,56 @@ object ScalaDensity {
     f.counts(node)/(f.boxes(node).volume * f.counts(1))
   }
 
+  // NOTE: This is the quantile thing
+  case class Quantiles( splits : FiniteTree[(Double, Int)],
+                        probs  : FiniteTree[Double] )
+
+  def densityAtNode(h : Histogram, lab : NodeLabel) : Double = {
+    h.counts(lab)/(h.boxes(lab).volume * h.counts(1))
+  }
+
+  def probAtNode(h : Histogram, lab : NodeLabel) : Double = {
+    h.counts(lab)/(h.counts(1) * 1.0)
+  }
+
+  def toQuantiles(h : Histogram) : Quantiles = {
+    val splits = h.splits
+    val n = h.counts(rootLabel)
+    val leafHeights  = h.leaves.toList.map {
+      case lab =>
+        (lab, densityAtNode(h, lab), h.counts(lab))
+    }
+
+    val cumulHeights = leafHeights.
+      sortWith(_._2 < _._2).
+      scanLeft((rootLabel, 0L))(
+        { case ((_, cumc), (lab, _, c)) => (lab, cumc + c) }).
+      map( { case (lab, cumc) => (lab, cumc/(n*1.0)) } ).
+      tail.toMap
+
+    Quantiles(splits, cumulHeights)
+  }
+
+  def queryQuantile(q : Quantiles, p : MLVector) : Double = {
+    var node = BigInt(1)
+    val splits = q.splits
+    while(splits.isDefinedAt(node)) {
+      val (t, d) = splits(node)
+      if(p(d) < t) {
+        node = left(node)
+      } else {
+        node = right(node)
+      }
+    }
+    q.probs(node)
+  }
+
   // NOTE: This is only approximate since we do not collapse cherries
   //       that no longer satisfy the splitting criterion after removing
   //       the point.
   def looL2ErrorApprox(f : Histogram) : Double = {
     val norm = f.counts(1)
-    f.leaves.map{
+    f.leaves.toList.map{
         x : NodeLabel =>
         val c = f.counts(x)
         val v = f.boxes(x).volume
@@ -268,6 +338,10 @@ object ScalaDensity {
   //   node
   // }
 
+  def isRight(x : NodeLabel) : Boolean =  x.testBit(0)
+  def  isLeft(x : NodeLabel) : Boolean = !x.testBit(0)
+  def parent(x : NodeLabel) : NodeLabel = x >> 1
+
   def depthFirst[A](t : FiniteTree[A]) : Stream[(Int, NodeLabel, A)] = {
     def go(lev : Int, lab : NodeLabel) : Stream[(Int, NodeLabel, A)] = {
       if(t.isDefinedAt(lab))
@@ -281,9 +355,53 @@ object ScalaDensity {
   // NOTE: Warning, does not check to make sure something is a cherry!
   def cutCherry(h : Histogram, lab : BigInt) : Histogram = {
     // TODO: Should the others be pruned as well, to conserve memory?
-    Histogram( h.boxes, h.splits - lab, h.counts,
-               h.leaves + lab - left(lab) - right(lab) )
+    // print("Cutting ")
+    // println(lab)
+    val result =
+      Histogram( h.boxes, h.splits - lab, h.counts,
+                ((h.leaves + lab) - left(lab)) - right(lab) )
+    // if(!h.splits.isDefinedAt(lab)) {
+    //   println(lab)
+    //   println(h.counts(lab))
+    //   println(h.counts(left(lab)))
+    //   println(h.counts(right(lab)))
+    //   println("CUTTING NON-INTERNAL NODE!")
+    //   println(result.leaves.toList.map(result.counts(_)).sum)
+    //   System.exit(1)
+    // } else if(!h.leaves.contains(left(lab)) && !h.leaves.contains(right(lab))) {
+    //   println(lab)
+    //   println(h.counts(lab))
+    //   println(h.counts(left(lab)))
+    //   println(h.counts(right(lab)))
+    //   println("CUTTING WITH MISSING LEAVES!!!")
+    //   println(result.leaves.toList.map(result.counts(_)).sum)
+    //   System.exit(1)
+    // } else if(!h.leaves.contains(left(lab))) {
+    //   println(lab)
+    //   println(h.counts(lab))
+    //   println(h.counts(left(lab)))
+    //   println(h.counts(right(lab)))
+    //   println("CUTTING WITH MISSING LEFT LEAF!!!")
+    //   println(left(lab))
+    //   println(result.leaves.toList.map(result.counts(_)).sum)
+    //   println(result.leaves.toList.filter(isAncestorOf(left(lab), _)))
+    //   System.exit(1)
+    // } else if(!h.leaves.contains(right(lab))) {
+    //   println(lab)
+    //   println(h.counts(lab))
+    //   println(h.counts(left(lab)))
+    //   println(h.counts(right(lab)))
+    //   println("CUTTING WITH MISSING RIGHT LEAF!!!")
+    //   println(right(lab))
+    //   println(result.leaves.toList.map(result.counts(_)).sum)
+    //   println(result.leaves.toList.filter(isAncestorOf(right(lab), _)))
+    //   System.exit(1)
+    // }
+    result
   }
+
+  def ancestors(x : NodeLabel) : Stream[NodeLabel] =
+    Stream.iterate(x)(parent).takeWhile(_ >= rootLabel).tail
 
   def backtrack[H]( hs : Histogram,
                     prio : (Int, NodeLabel, Long, Double) => H)(implicit ord : Ordering[H])
@@ -301,11 +419,43 @@ object ScalaDensity {
     }
 
     var q = new PriorityQueue()(BacktrackOrder) //(Ordering[H].on({x : (H,NodeLabel) => x._1}))
+
+    // TESTS
+    // val s1 = depthFirst(hs.splits).map({ case (_, lab, _) => lab }).toSet
+
+    // for(l <- hs.leaves) {
+    //   for(a <- ancestors(l)) {
+    //     if(!s1.any(isAncestorOf(_, a))) {
+    //       println("Missing ancestor!!!")
+    //       System.exit(1)
+    //     }
+    //   }
+    // }
+
+    // for(l <- s1) {
+    //   if(!hs.leaves.any(isAncestorOf(l, _))) {
+    //     println("Non-ancestral internal node!!!")
+    //     System.exit(1)
+    //   }
+    // }
+
+    // for(l <- hs.leaves) {
+    //   if(hs.leaves.any(x => x != l && isAncestorOf(x, l))) {
+    //     println("Non-leaf leaf!!!!")
+    //     println(l)
+    //     println(hs.leaves.filter(isAncestorOf(_, l)))
+    //     System.exit(1)
+    //   }
+    // }
+
     depthFirst(hs.splits).foreach {
       case (lev, lab, _) =>
         q += ((prio(lev, lab, hs.counts(lab), hs.boxes(lab).volume), lab))
     }
     val sorted : Stream[(H, NodeLabel)] = q.dequeueAll
+
+    // print(sorted.toList)
+
     sorted.scanLeft((hs, 0L, 0L, 0.0, 0.0)) {
       case ((hc, _, _, _, _), (_, lab)) =>
         ( cutCherry(hc, lab),
@@ -315,6 +465,16 @@ object ScalaDensity {
           hs.boxes(right(lab)).volume )
     }.tail
   }
+
+  def logQuasiLik(h : Histogram) : Double =
+    h.leaves.toList.map({
+      case lab =>
+        val c = h.counts(lab)
+        if(c == 0) 0 else c*log(densityAtNode(h, lab))
+    }).sum
+
+  def logPenalisedQuasiLik(h : Histogram, taurec : Double) : Double =
+    log(exp(taurec) - 1) - h.leaves.size*taurec + logQuasiLik(h)
 
   def main(args: Array[String]) = {
     Logger.getLogger("org").setLevel(Level.ERROR)
@@ -327,8 +487,8 @@ object ScalaDensity {
 
     val df = normalVectorRDD(sc, 200, 2)
 
-    val hs1 = supportCarved(df, 5, 0.0005)
     val hs2 = histogram(df, 10, 0.0005)
+    val hs1 = supportCarved(df, 5, 0.0005)
 
     val hs2path = backtrack(hs2, { case (_, _, count, _) => count })(Ordering[Long].reverse)
 
@@ -349,16 +509,44 @@ object ScalaDensity {
           mark(Point).
           html.pageHTML().getBytes(StandardCharsets.UTF_8))
 
+    def dumpQuant(qs : Quantiles, path : String) : Unit =
+      Files.write(
+        Paths.get(path),
+        Vegas("Histogra").
+          withData( df.collect().toSeq.map(x =>
+                     Map("x" -> x(0), "y" -> x(1),
+                         "n" -> queryQuantile(qs, Vectors.dense(x(0), x(1)))
+                     )
+                   ) ).
+          encodeX("x", Quant).
+          encodeY("y", Quant).
+          encodeOpacity("n", Quantitative).
+          mark(Point).
+          html.pageHTML().getBytes(StandardCharsets.UTF_8))
+
     var count = 1
+    val temperature = 1.0
     dumpHist(hs2, "tmp0.html")
-    println(looL2ErrorApprox(hs2))
     for((hs, _, _, _, _) <- hs2path) {
       // TODO: implement an optimised version that recomputes the the error
       // based on the previous estimate and count/volume for the collapsed cells
+      print("LOO error estimate:")
       println(looL2ErrorApprox(hs))
+      print("Penalised Quasi-likelihood: ")
+      println(exp(logPenalisedQuasiLik(hs, 1/temperature)))
+      // Compute tail probabilities
+      val qs = toQuantiles(hs)
+      dumpQuant(qs, "tmpquant" + count.toString + ".html")
+      print("Tail probability (0, 0): ")
+      println(queryQuantile(qs, Vectors.dense(0.0, 0.0)))
+      print("Tail probability (3, 3): ")
+      println(queryQuantile(qs, Vectors.dense(3.0, 3.0)))
       dumpHist(hs, "tmp" + count.toString + ".html")
       count = count + 1
+      println("")
     }
+
+
 
     // dumpHist(hs1, "tmp1.html")
     // dumpHist(hs2, "tmp2.html")
